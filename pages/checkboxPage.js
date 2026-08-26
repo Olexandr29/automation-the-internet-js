fe760959d4d27f5c482d7112ad594523694ab6ec
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2026-08-26 14:35:02 +0300

    feat: add automated tests for TC32-35

--- pages/checkboxPage.js
+++ pages/checkboxPage.js
@@ -1,5 +1,5 @@
 const BasePage = require("./basePage");
-const { By } = require('selenium-webdriver');
+const { By, ActionChains, Key } = require('selenium-webdriver');
 
 class CheckboxPage extends BasePage{
     constructor(driver) {
@@ -14,6 +14,36 @@ class CheckboxPage extends BasePage{
         const checkboxes = await this.driver.findElements(this.locators.checkboxesLocator);
         return await checkboxes[checkboxNumber - 1].isDisplayed();
     }
+
+    async isCheckboxChecked(checkboxNumber) {
+        const checkboxes = await this.driver.findElements(this.locators.checkboxesLocator);
+        return await checkboxes[checkboxNumber - 1].isSelected();
+    }
+
+    async changeCheckboxState(checkboxNumber) {
+        const checkboxes = await this.driver.findElements(this.locators.checkboxesLocator);
+        return await checkboxes[checkboxNumber - 1].click();
+    }
+
+    async focusCheckbox(checkboxNumber) {
+        const checkboxes = await this.driver.findElements(this.locators.checkboxesLocator);
+        const targetCheckbox = checkboxes[checkboxNumber - 1];
+        const targetCheckboxId = await targetCheckbox.getId();
+        for (let i = 0; i < 10; i++) {
+            await this.driver.actions().sendKeys(Key.TAB).perform();
+            const activeElement = await this.driver.switchTo().activeElement();
+            const activeElementId = await activeElement.getId();           
+            if (await activeElementId === targetCheckboxId) {
+                return;
+            }
+        }
+        throw new Error(`Checkbox ${checkboxNumber} could not be focused using Tab`);
+    }
+
+    async changeCheckboxStateBySpaceKey() {
+        await this.driver.actions().sendKeys(Key.SPACE).perform();
+    }
+
 
 }
 
--- tests/checkboxTest.js
+++ tests/checkboxTest.js
@@ -4,24 +4,59 @@ const { createDriver, closeDriver } = require('./testSetup');
 const assert = require('assert')
 const CheckboxData = require('../testData/checkboxData');
 
-describe("[Regression] Checkbox test suite", function() {
+describe("[Regression] Checkbox test suite", function () {
     let driver;
-    let homePage, checboxPage;
+    let homePage, checkboxPage;
 
-    beforeEach(async function() {
+    beforeEach(async function () {
         driver = await createDriver();
         homePage = new HomePage(driver);
+        checkboxPage = await homePage.openCheckboxPage();
+
     });
 
-    afterEach(async function() {
+    afterEach(async function () {
         await closeDriver(driver, this.currentTest);
     });
 
-    it("[Smoke] TC31 - Verify Checkboxes are visible", async function() {
-        checboxPage = await homePage.openCheckboxPage();
+    it("[Smoke] TC31 - Verify Checkboxes are visible", async function () {
         assert.strictEqual(await driver.getCurrentUrl(), CheckboxData.URL, "The Checkboxes page is not opened");
-        assert.strictEqual(await checboxPage.isCheckboxVisible(1), true, `The ${CheckboxData.CHECKBOX_1} is not visible`);
-        assert.strictEqual(await checboxPage.isCheckboxVisible(2), true, `The ${CheckboxData.CHECKBOX_2} is not visible`);
-    })
-    
+        assert.strictEqual(await checkboxPage.isCheckboxVisible(1), true, `The ${CheckboxData.CHECKBOX_1} is not visible`);
+        assert.strictEqual(await checkboxPage.isCheckboxVisible(2), true, `The ${CheckboxData.CHECKBOX_2} is not visible`);
+    });
+
+    it("TC32 - Verify Checkboxes initial state", async function() {
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(1), false, `The ${CheckboxData.CHECKBOX_1} is not unchecked`);
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(2), true, `The ${CheckboxData.CHECKBOX_2} is not checked`);
+    });
+
+    it("[Smoke] TC33 - Verify checkboxes state changes correctly", async function () {
+        await checkboxPage.changeCheckboxState(1);
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(1), true, `The ${CheckboxData.CHECKBOX_1} is not checked`);
+        await checkboxPage.changeCheckboxState(1);
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(1), false, `The ${CheckboxData.CHECKBOX_1} is not unchecked`);
+        await checkboxPage.changeCheckboxState(2);
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(2), false, `The ${CheckboxData.CHECKBOX_2} is not unchecked`);
+        await checkboxPage.changeCheckboxState(2);
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(2), true, `The ${CheckboxData.CHECKBOX_2} is not checked`);
+    });
+
+    it("TC34 - Verify checkboxes state after refresh", async function() {
+        await checkboxPage.changeCheckboxState(1);
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(1), true, `The ${CheckboxData.CHECKBOX_1} is not chacked`);
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(2), true, `The ${CheckboxData.CHECKBOX_2} is not checked`);
+        await driver.navigate().refresh();
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(1), false, `The ${CheckboxData.CHECKBOX_1} is checked`);
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(2), true, `The ${CheckboxData.CHECKBOX_2} is not checked`);
+    });
+
+    it("TC35 - Verify checkbox state chnges using keyboard", async function() {
+        await checkboxPage.focusCheckbox(1);
+        await checkboxPage.changeCheckboxStateBySpaceKey();
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(1), true, `The ${CheckboxData.CHECKBOX_1} is not checked`);
+        await checkboxPage.focusCheckbox(2);
+        await checkboxPage.changeCheckboxStateBySpaceKey();
+        assert.strictEqual(await checkboxPage.isCheckboxChecked(2), false, `The ${CheckboxData.CHECKBOX_2} is not unchecked`);
+    });
+
 })
